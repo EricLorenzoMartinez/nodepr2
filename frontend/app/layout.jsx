2e4cad9cee67b0dc867d29f5b40d971f2ff133aa
Eric <eric.lorenzo@gracia.lasalle.cat>
2026-05-09 15:25:36 +0200

    Revert "feat: add login page with API interaction"
    This reverts commit ae96f95b2a2056a17c5bc003f26f1aac9dacdfb7.

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { AuthProvider } from './_context/AuthContext';
+import { AuthProvider } from './_context/AuthProvider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
